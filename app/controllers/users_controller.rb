650b9f6a6bac735a25b0638995c3d5bee8a79237
Ashley <a.clarkem@gmail.com>
2014-10-03 17:35:39 -0600

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,25 @@ class UsersController < ApplicationController
   skip_before_action :ensure_current_user, only: [:new, :create]
 
 
-
   def create
     @user = User.create(email: params[:email])
-    @user.save!
-    redirect_to messages_path
+    espond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver
+        redirect_to messages_path
 
+      end
+    end
+
+    def edit
+
+    end
+
+
+    def update
+
+
+    end
 
   end
-
-  def edit
-
-  end
-
-
-  def update
-
-
-  end
-
-end
