854b2b7cc44b1efc0be31cf3915494f1b54bff73
Ashley <a.clarkem@gmail.com>
2014-10-03 17:33:09 -0600

    merging with seth

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,5 @@ class MessagesController < ApplicationController
 
 
 
+
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "motivation@life.rocks"
+
+  def motivational_email(user)
+    @user = user
+    @url = 'http://localhost:3000/'
+    mail(to: @user.email, subject: "Your Motivational Quote")
+  end
+end
